0e75c03c333fac9866632c15678751c6e9d68af5
Michael Sisson <88689056+notmikesisson@users.noreply.github.com>
2021-08-09 23:12:03 +0000

    Started Project Draft

---
+++ main.py
@@ -0,0 +1,17 @@
+#Author: Mike Sisson
+#Created Aug 9, 2021
+#This is a weather checking app. It prompts the user for the zip code and returns the weather after retrieving information from the server
+
+#Display welcome message
+
+#Prompt for zip code or city. If City prompt for state
+
+#Connect to server to retrieve information. Display connecting during process
+
+#If unable to connect ask to retry. 
+
+#Once connected, retrieve and store the weather info
+
+#Display weather info
+
+#Prompt to Refresh, quit program or try a new location
