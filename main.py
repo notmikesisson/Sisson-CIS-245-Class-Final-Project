889cd6fb404ad415a21410ecd30cc4f509f510ee
Michael Sisson <88689056+notmikesisson@users.noreply.github.com>
2021-08-12 21:11:12 +0000

    pulled temp variables and set to print

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Author: Mike Sisson
 #Created Aug 9, 2021
 #This is a weather checking app. It prompts the user for the zip code and returns the weather after retrieving information from the server
-
+import requests
 api = "f8647affb7675ef8cd050e699a5d22e8"
 website = "http://api.openweathermap.org/"
 
@@ -10,11 +10,25 @@ webByCityState1 = 'api.openweathermap.org/data/2.5/weather?q='
 webByCityState2 = '&appid=f8647affb7675ef8cd050e699a5d22e8'
 
 webByZip1 = "api.openweathermap.org/data/2.5/weather?zip=" 
-#enter zip code between two strings in curly brackets (zip code)
+#enter zip code between two strings in curly brackets {zip code}
 webByZip2 = ",us&appid=f8647affb7675ef8cd050e699a5d22e8"
 #Display welcome message
 
+response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Omaha,us&units=imperial&APPID=f8647affb7675ef8cd050e699a5d22e8")
+js = response.json()
 
+
+print(js)
+temperature = js['main']['temp']
+highTemp = js['main']['temp_min']
+lowTemp = js['main']['temp_max']
+feelsLike = js['main']['feels_like']
+print(temperature)
+print("It feels like " + str(feelsLike) + " degrees.")
+print("Low: " + str(lowTemp))
+print("High: " + str(highTemp))
+
+'''
 #Prompt for zip code or city. If City prompt for state
 looping = True
 while looping == True:
@@ -26,7 +40,7 @@ while looping == True:
     state = input("What state are you from?: ")
   else:
     print('Please enter 1 or 2')
-
+'''
 #Connect to server to retrieve information. Display connecting during process
 print('Connecting to server...')
 
